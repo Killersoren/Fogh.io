c3eb20662711bdbcf4ebb57530868d6e2381bced
Killersoren <skmj@hotmail.dk>
2025-07-16 02:12:16 +0200

    version 6 personalized

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Esben Fogh – Software Engineer";
+export const SITE_DESCRIPTION = "Diplomingeniør i Software Teknologi specialising in RPA, automation and full-stack development.";
